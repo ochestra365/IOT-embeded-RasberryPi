6dfd68e78aac24c46aa5dc4cfb0baf85e7d73eed
ochestra365@naver.com <ochestra365@naver.com>
2021-07-13 12:24:37 +0900

    commit

--- test06.py
+++ test06.py
@@ -1,13 +1,17 @@
 try:
     f = open('IOT-embeded2-RasberryPi/data/readme.txt',mode='r',encoding='utf-8')# 텍스트 파일 오픈
-
+    f2= open('IOT-embeded2-RasberryPi/data/writeme.txt',mode='w',encoding='utf-8')#작성파일 오픈
     line=f.read()#readline()
     while line:
         print(line)
+        f2.wrtie(line)
         line=f.read()
+    f2.write("추가내용")
     f.close() # 파일 닫기
+    f2.close()
 
-except FileNotFoundError as e:
+    print('파일 작성 완료')
+except Exception as e:
     print('파일 경로가 틀렸는데여 : {0}'.format(e))
 
 #예외는 실행중에 발생한 오류를 의미한다.
---
+++ test07.py
@@ -0,0 +1,6 @@
+import RPi.GPIO as GPIO
+import minimum as Min
+
+value = Min.min(10,5)
+print(value)
+#C#은 이런 내용들이 dll로 되어 있다. 
